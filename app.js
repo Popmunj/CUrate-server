4e3dcff2f057bc313ecc78d0088fc118815274c7
Pawarisa Munjitjuntra <pop@Pawarisas-MacBook-Air.local>
2025-05-15 08:12:10 +0700

    /

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-// const cors = require('cors');
+const router = express.Router();
 
 const restaurantRoute = require('./routes/restaurant');
 // const aiRoute = require('./routes/restaurants');
 
 
-const app = express();
 
-// app.use(cors());
+
+const app = express();
 app.use(express.json());
+
 
 app.use('/restaurant', restaurantRoute);
 // app.use('/ai', aiRoute);
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found', path: req.path });
+  });
 
 
 module.exports = app;
