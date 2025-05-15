595c849914165151d37c8a436420b77b7789a0d0
Pawarisa Munjitjuntra <pop@Pawarisas-MacBook-Air.local>
2025-05-15 07:40:32 +0700

    Firebase key handling

--- services/db.js
+++ services/db.js
@@ -1,6 +1,11 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('../db-keys.json');
+// const serviceAccount = require('../db-keys.json');
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_KEY_B64, 'base64').toString('utf8')
+);
+
 
 initializeApp({
   credential: cert(serviceAccount),
