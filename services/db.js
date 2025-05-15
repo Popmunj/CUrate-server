d268f664c961434412f86084253e4421c3038827
Pawarisa Munjitjuntra <pop@Pawarisas-MacBook-Air.local>
2025-05-15 08:30:53 +0700

    db

--- services/db.js
+++ services/db.js
@@ -2,14 +2,20 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 // const serviceAccount = require('../db-keys.json');
 
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_KEY_B64, 'base64').toString('utf8')
-);
+try {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_KEY_B64, 'base64').toString('utf8')
+  );
 
 
-initializeApp({
-  credential: cert(serviceAccount),
-});
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
 
-const db = getFirestore();
-module.exports = db;
+  const db = getFirestore();
+  module.exports = db;
+
+} catch (err) {
+  console.log(err);
+}
+
